f40d7271be20df090a830c99c32e6776b4906dee
Shamoun <shamoun.yousuf04@gmail.com>
2024-01-13 04:36:06 -0500

    Added a little features

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -15,7 +16,8 @@ class Category(models.Model):
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text = RichTextField(blank=True, null=True)
+    #text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     category = models.CharField(max_length=200, default = 'no category')
