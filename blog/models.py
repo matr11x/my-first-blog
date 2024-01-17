a4fb47f52ffff70efca01381ce7c0052f4887f47
Shamoun <shamoun.yousuf04@gmail.com>
2024-01-17 03:12:08 -0500

    Added comments section to posts

--- blog/models.py
+++ blog/models.py
@@ -31,3 +31,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name = "comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length = 255)
+    body = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
